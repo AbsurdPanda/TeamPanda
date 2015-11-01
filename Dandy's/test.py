4c0fe14d0a18d2ec6deefa87d1d40f8fd6e31a35
DanDPanda <Dan@Dans-Air-5.local.tld>
2015-10-31 19:47:58 -0500

    Added stuff

--- Dandy's/test.py
+++ Dandy's/test.py
@@ -1,5 +1,20 @@
-# <- is used to comment code, for others to understand
-print "Hello, Python!"
-
-for x in range(1,5):
-	print "Hello"
+print"Hello, for this test, we are going to have you put in two numbers, and choose to, add, subtract, multiply or divid the two numbers."
+firstnum = raw_input("What is your first number? - ")
+if firstnum.isdigit() != True:
+	print"Error, try again."
+else:	
+	secondnum = raw_input("WHat is your second number? - ")
+	if secondnum.isdigit() != True:
+		print"Error, try again."
+	else:
+		op = raw_input("Choose to either (+, -, /, or *): ")
+		if op == '+':
+			print(int(firstnum) + int(secondnum))
+		elif op == '-':
+			print(int(firstnum) - int(secondnum))
+		elif op == '/':
+			print(int(firstnum) / int(secondnum))
+		elif op == '*':
+			print(int(firstnum) * int(secondnum))
+		else:
+			print"Error, try again."
